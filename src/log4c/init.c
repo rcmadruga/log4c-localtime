acbaee34bcb1881db97969dd2c411446f32ca4cc
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2014-10-25 19:45:33 +0200

    Fix debug mode build with uClibc
    When --enable-debug is passed, liblog4c-localtime wants to use
    <mcheck.h>. It takes the precaution of testing if __GLIBC__ is
    defined. But unfortunately, the uClibc C library pretends to be
    compatible with glibc by defining __GLIBC__, but it doesn't provide
    mcheck.h.
    
    To better support this situation, we add an AC_CHECK_HEADERS() check
    on mcheck.h, and then use HAVE_MCHECK_H were appropriate.
    
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- src/log4c/init.c
+++ src/log4c/init.c
@@ -35,7 +35,7 @@ static const char version[] = "$Id$";
 #include <layout_type_basic_r.h>
 #include <layout_type_dated_r.h>
 
-#if defined(__LOG4C_DEBUG__) && defined(__GLIBC__)
+#if defined(__LOG4C_DEBUG__) && defined(HAVE_MCHECK_H)
 #include <mcheck.h>
 #endif
 
@@ -100,7 +100,7 @@ extern int log4c_init(void)
     sd_debug("log4c_init[");
   
     /* activate GLIBC allocation debugging */
-#if defined(__LOG4C_DEBUG__) && defined(__GLIBC__)
+#if defined(__LOG4C_DEBUG__) && defined(HAVE_MCHECK_H)
     mtrace();
 #endif
   
@@ -280,7 +280,7 @@ extern int log4c_fini(void)
 	log4c_dump_all_instances(stderr);
     }
 #endif
-#if defined(__LOG4C_DEBUG__) && defined(__GLIBC__)
+#if defined(__LOG4C_DEBUG__) && defined(HAVE_MCHECK_H)
     muntrace();
 #endif
   
